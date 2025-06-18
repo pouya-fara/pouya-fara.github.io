b9afd3c2e91ab989dc552c11afd45eba01db1e66
Pouya <pouyafarahani83@gmail.com>
2025-06-17 22:58:13 -0400

    Bugs fixed

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ document.querySelector("html").style.height = window.innerHeight + "px";
 document.querySelector("body").style.height = window.innerHeight + "px";
 document.querySelector("#loading").style.height = window.innerHeight + "px";
 document.querySelector("#web-content").style.height = window.innerHeight + "px";
-document.querySelector("#menu-list").style.height = window.innerHeight + "px";
 
 // preloading
 window.onload = function(){
